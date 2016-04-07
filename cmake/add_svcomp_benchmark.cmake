a5378b9bf27e2e3a6812aea5163243e417c03f23
Dan Liew <daniel.liew@imperial.ac.uk>
2016-04-07 13:19:38 +0100

    Teach the build system to only re-generate the CMake commands to build a benchmark when necessary (i.e. when the ``spec.yml`` file changes). When we have many benchmarks this optimisation would likely incremental builds faster (without this optimisation re-configure would be slow).

--- cmake/add_svcomp_benchmark.cmake
+++ cmake/add_svcomp_benchmark.cmake
@@ -1,19 +1,21 @@
 macro(add_svcomp_benchmark BENCHMARK_DIR)
-  # FIXME: Check timestamp so we don't have to regenerate every file
-  # for incremental builds
   set(INPUT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/${BENCHMARK_DIR}/spec.yml)
   set(OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/${BENCHMARK_DIR}_targets.cmake)
-  execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/svcb-emit-cmake-decls.py
-                          ${INPUT_FILE}
-                          --architecture ${SVCOMP_ARCHITECTURE}
-                          --output ${OUTPUT_FILE}
-                  RESULT_VARIABLE RESULT_CODE
-                 )
-  if (NOT ${RESULT_CODE} EQUAL 0)
-    message(FATAL_ERROR "Failed to process benchmark ${BENCHMARK_DIR}. With error ${RESULT_CODE}")
+  # Only re-generate the file if necessary so that re-configure is as fast as possible
+  if ("${INPUT_FILE}" IS_NEWER_THAN "${OUTPUT_FILE}")
+    message(STATUS "Generating \"${OUTPUT_FILE}\"")
+    execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/svcb-emit-cmake-decls.py
+                            ${INPUT_FILE}
+                            --architecture ${SVCOMP_ARCHITECTURE}
+                            --output ${OUTPUT_FILE}
+                    RESULT_VARIABLE RESULT_CODE
+                   )
+    if (NOT ${RESULT_CODE} EQUAL 0)
+      message(FATAL_ERROR "Failed to process benchmark ${BENCHMARK_DIR}. With error ${RESULT_CODE}")
+    endif()
   endif()
   # Include the generated file
   include(${OUTPUT_FILE})
   # Let CMake know that configuration depends on the benchmark specification file
-  set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${INPUT_FILE})
+  set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${INPUT_FILE}")
 endmacro()
