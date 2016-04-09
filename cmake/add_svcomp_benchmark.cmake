delcypher/symex-fp-bench

commit a5378b9bf27e2e3a6812aea5163243e417c03f23

Teach the build system to only re-generate the CMake commands to build a benchmark when necessary (i.e. when the ``spec.yml`` file changes). When we have many benchmarks this optimisation would likely incremental builds faster (without this optimisation re-configure would be slow).

cmake/add_svcomp_benchmark.cmake
@@ -1,19 +1,21 @@
 macro(add_svcomp_benchmark BENCHMARK_DIR)
-  # FIXME: Check timestamp so we don't have to regenerate every file
-  # for incremental builds
   set(INPUT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/${BENCHMARK_DIR}/spec.yml)
   set(OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/${BENCHMARK_DIR}_targets.cmake)
-  execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/svcb-emit-cmake-decls.py
-                          ${INPUT_FILE}
-                          --architecture ${SVCOMP_ARCHITECTURE}
-                          --output ${OUTPUT_FILE}
-                  RESULT_VARIABLE RESULT_CODE
-                 )
-  if (NOT ${RESULT_CODE} EQUAL 0)
-    message(FATAL_ERROR "Failed to process benchmark ${BENCHMARK_DIR}. With error ${RESULT_CODE}")
+  # Only re-generate the file if necessary so that re-configure is as fast as possible
+  if ("${INPUT_FILE}" IS_NEWER_THAN "${OUTPUT_FILE}")
+    message(STATUS "Generating \"${OUTPUT_FILE}\"")
+    execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/svcb-emit-cmake-decls.py
+                            ${INPUT_FILE}
+                            --architecture ${SVCOMP_ARCHITECTURE}
+                            --output ${OUTPUT_FILE}
+                    RESULT_VARIABLE RESULT_CODE
+                   )
+    if (NOT ${RESULT_CODE} EQUAL 0)
+      message(FATAL_ERROR "Failed to process benchmark ${BENCHMARK_DIR}. With error ${RESULT_CODE}")
+    endif()
   endif()
   # Include the generated file
   include(${OUTPUT_FILE})
   # Let CMake know that configuration depends on the benchmark specification file
-  set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${INPUT_FILE})
+  set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${INPUT_FILE}")
 endmacro()

commit d243a464fa52f0b1fe51200f4296487c810b93c2

Fix bug introduced by a5378b9bf27e2e3a6812aea5163243e417c03f23
If the python files and schema were modifed the CMake files that contain
the generated targets would not be recreated. Now the CMake code
explicitly maintains a list of files that the generation of the target
files depends on so that if any of those files change regeneration
is forced.

cmake/add_svcomp_benchmark.cmake
@@ -1,8 +1,30 @@
+# List of additional files that generation of target
+# include files should depend on. This ensures that if these
+# files change the files containing the benchmark targets
+# get regenerated (i.e. OUTPUT_FILE in add_svcomp_benchmark())
+set(SVCOMP_ADDITIONAL_GEN_CMAKE_INC_DEPS
+  "${CMAKE_SOURCE_DIR}/svcb/benchmark.py"
+  "${CMAKE_SOURCE_DIR}/svcb/build.py"
+  "${CMAKE_SOURCE_DIR}/svcb/schema.py"
+  "${CMAKE_SOURCE_DIR}/svcb/schema.yml"
+  "${CMAKE_SOURCE_DIR}/svcb/util.py"
+  "${CMAKE_SOURCE_DIR}/svcb-emit-cmake-decls.py"
+)
+
 macro(add_svcomp_benchmark BENCHMARK_DIR)
   set(INPUT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/${BENCHMARK_DIR}/spec.yml)
   set(OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/${BENCHMARK_DIR}_targets.cmake)
   # Only re-generate the file if necessary so that re-configure is as fast as possible
-  if ("${INPUT_FILE}" IS_NEWER_THAN "${OUTPUT_FILE}")
+  set(_should_force_regen FALSE)
+  foreach (dep ${SVCOMP_ADDITIONAL_GEN_CMAKE_INC_DEPS})
+    if (NOT EXISTS "${dep}")
+      message(FATAL_ERROR "Dependency \"${dep}\" does not exist")
+    endif()
+    if ("${dep}" IS_NEWER_THAN "${OUTPUT_FILE}")
+      set(_should_force_regen TRUE)
+    endif()
+  endforeach()
+  if (("${INPUT_FILE}" IS_NEWER_THAN "${OUTPUT_FILE}") OR ${_should_force_regen})
     message(STATUS "Generating \"${OUTPUT_FILE}\"")
     execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_SOURCE_DIR}/svcb-emit-cmake-decls.py
                             ${INPUT_FILE}
@@ -18,4 +40,8 @@ macro(add_svcomp_benchmark BENCHMARK_DIR)
   include(${OUTPUT_FILE})
   # Let CMake know that configuration depends on the benchmark specification file
   set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${INPUT_FILE}")
+  foreach (dep ${SVCOMP_ADDITIONAL_GEN_CMAKE_INC_DEPS})
+    set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${dep}")
+  endforeach()
+  unset(_should_force_regen)
 endmacro()