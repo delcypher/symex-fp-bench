84a42e3bfb04d1b246f9e145815e61fe37b25510
Dan Liew <daniel.liew@imperial.ac.uk>
2016-09-08 12:14:31 +0100

    Sketch an example benchmark. The build is currently broken because the build system doesn't know how to handle the `klee_runtime` dependency.

---
+++ benchmarks/c/examples/single_branch/main.c
@@ -0,0 +1,22 @@
+#ifdef KLEE
+#include "klee/klee.h"
+#endif
+#include <assert.h>
+#include <stdio.h>
+
+int main() {
+  int a;
+#ifdef KLEE
+  klee_make_symbolic(&a, sizeof(a), "a");
+#endif
+
+  if (a == 0) {
+    printf("a is zero\n");
+#ifdef BUG
+    assert(false);
+#endif
+  } else {
+    printf("a is non-zero\n");
+  }
+  return 0;
+}
