e257315fe4937f0dae730640e0509151ed21a550
Daniel Schemmel <daniel.schemmel@comsys.rwth-aachen.de>
2017-01-13 11:27:11 +0100

    a proper override for assert

--- overrides/include/assert.h
+++ overrides/include/assert.h
@@ -6,7 +6,7 @@ void __gcov_flush();
 void __assert_fail(char const* assertion, char const* file, unsigned int line, char const* function);
 #define assert(expr)                                          \
 	do {                                                        \
-		if(expr) {                                                \
+		if(!(expr)) {                                             \
 			__gcov_flush();                                         \
 			__assert_fail(#expr, __FILE__, __LINE__, __func__);     \
 		}                                                         \
