4009e56a6f305a77d20d9cf2ea3a06e91f338d77
Dan Liew <daniel.liew@imperial.ac.uk>
2017-01-24 21:36:11 +0000

    Change `assert.h` override to include the original `assert.h` header just in case clients are relying on something internal to the header.
    This makes the implementation of the override header very similar
    to the `stdlib.h` override.

--- overrides/include/assert.h
+++ overrides/include/assert.h
@@ -1,9 +1,31 @@
+// Undefine our assert macro just in case this header is included multiple times.
+#undef assert
+// GCC preprocessor specific behaviour to find next header in search path
+#if __GNUC__
+#include_next <assert.h>
+#else
+#error "Require gcc/clang"
+#endif
+
 #undef assert
 #ifdef NDEBUG
 #define assert(ignore) ((void)0)
 #else
-void __gcov_flush();
-void __assert_fail(char const* assertion, char const* file, unsigned int line, char const* function);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef SVCB_GCOV_FLUSH_DECL
+extern void __gcov_flush();
+#define SVCB_GCOV_FLUSH_DECL
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+// Modified version of assert() macro that flushes gcov coverage files first.
 #define assert(expr)                                          \
 	do {                                                        \
 		if(!(expr)) {                                             \
@@ -11,4 +33,4 @@ void __assert_fail(char const* assertion, char const* file, unsigned int line, c
 			__assert_fail(#expr, __FILE__, __LINE__, __func__);     \
 		}                                                         \
 	} while(0)
-#endif 
+#endif
