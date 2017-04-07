324384b631761fc034fafd80e8396e7d3d15203d
Dan Liew <daniel.liew@imperial.ac.uk>
2017-04-07 15:31:46 +0100

    Fix mistake in example benchmark that is supposed to be correct. The NaN and infinity cases were not considered.

--- benchmarks/c/examples/simple_math/main.c
+++ benchmarks/c/examples/simple_math/main.c
@@ -6,10 +6,14 @@
 #include "math.h"
 
 int main() {
-  float a;
+  float a = 0.0f;
 #ifdef KLEE
   klee_make_symbolic(&a, sizeof(a), "a");
 #endif
+  if (isnan(a) || isinf(a)) {
+    // assertion won't hold in these cases so exit early
+    return 0;
+  }
   float b = a - a;
   assert(sin(b) == 0.0f);
   return 0;
